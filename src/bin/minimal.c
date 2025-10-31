0f657f9c6bcd4f68db700853bdca182e8e17b330
Shane Snover <ssnover95@gmail.com>
2025-10-30 20:12:20 -0600

    blink gpio output pin 0

--- src/bin/minimal.c
+++ src/bin/minimal.c
@@ -2,12 +2,20 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-int main()
-{
-    while (true)
-    {
-        for (volatile int i = 0; i < 100000; ++i)
-        {
+int main() {
+    const uint32_t pin_number = 0;
+
+    // Set the pin to output
+    NRF_GPIO->DIRSET |= (1 << pin_number);
+    bool state = false;
+
+    while (true) {
+        state = !state;
+        if (state) {
+            NRF_GPIO->OUTSET |= (1 << pin_number);
+        } else {
+            NRF_GPIO->OUTCLR |= (1 << pin_number);
         }
+        for (volatile int i = 0; i < 1000000; ++i) {}
     }
 }
